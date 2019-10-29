df15187585e34ae0f99326032afbd2efe5524053
Mike Tong <tong_michael@bah.com>
2019-10-28 22:46:07 -0400

    added docs

--- scripts/ancillary.py
+++ scripts/ancillary.py
@@ -1,4 +1,10 @@
-#python3.6                                                         
+# python3.6                                                         
+"""./ancillary.py
+Contains helper functions for the notebooks
+
+CREATED AUG-2019: Mike Tong - tong_michael@bah.com
+"""
+import numpy as np
 
 
 def parse_webpage_table(table):                                    
@@ -25,7 +31,7 @@ def parse_webpage_table(table):
 
 
 def update_search_page(url, page):
-    """docstring"""
+    """docstring tbd"""
     
     url_num = page * 120
 
@@ -41,6 +47,12 @@ def update_search_page(url, page):
 
 
 def get_num_images(driver, url):
+    """Calculates the number of images on a CL posting
+    :param driver: The current webdriver class
+    :param url: The url to count images from
+    
+    :return: The number of images on a CL posting as an int
+    """
     driver.get(url)
     try:
         image_def = driver.find_element_by_xpath('/html/body/section/section/section/figure').get_attribute('class')
@@ -54,10 +66,19 @@ def get_num_images(driver, url):
 
 
 def get_education(driver, url):
+    """Pulls education data from the webpage
+    :param driver: The current webdriver class
+    :param url: The url to pull education data from
+    
+    :return: The craigslist education level
+    """
     driver.get(url)
     try:
-        paths = driver.find_elements_by_xpath('/html/body/section/section/section/div[1]/p/span')
-        education = [i.text for i in paths if i.text.find('education') != -1]  # sometimes the last item is a liscense
+        paths = driver.find_elements_by_xpath(
+            '/html/body/section/section/section/div[1]/p/span')
+        
+        # sometimes the last item is a license
+        education = [i.text for i in paths if i.text.find('education') != -1]
         if len(education) == 0:
             return education[0].split(':')[-1].strip()
         else:
